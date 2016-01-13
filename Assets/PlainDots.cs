a4639fbdae38e30a732fdb690ced0faf0742ce03
her0m31 <eda.1380@gmail.com>
2016-01-13 11:57:00 +0900

    物理特性を与えるタイミングを調整

--- Assets/PlainDots.cs
+++ Assets/PlainDots.cs
@@ -18,19 +18,26 @@ public class PlainDots : MonoBehaviour {
 			Vector3 newDotPosition = new Vector3(clickp.x, clickp.y, 0.0f);
 			parent = new GameObject("parent");
 			parent.transform.position = newDotPosition;
-			parent.AddComponent<Rigidbody2D>();
 		}
 
 		if (Input.GetMouseButton(0)) {
 			Vector3 clickp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 			Vector3 newDotPosition = new Vector3(clickp.x, clickp.y, 0.0f);
 
-			MakeDot(newDotPosition);
+			if(newDotPosition != lastDotPosition) {
+				MakeDot(newDotPosition);
+			}
+		}
+
+		if (Input.GetMouseButtonUp(0)) {
+			parent.AddComponent<Rigidbody2D>();
 		}
 	}
 
 	void MakeDot(Vector3 newDotPosition) {
 		Transform dot = (Transform) Instantiate(DotPrefab, newDotPosition, Quaternion.identity);
 		dot.parent = parent.transform;
+
+		lastDotPosition = newDotPosition;
 	}
 }
